f610d94ea43d7a11988e3da6f6a57691ab74df30
Sean Townsend <seane630@Seans-Mac-mini.local>
2018-02-07 12:50:29 -0600

    Created chatMessage class. Defines the types of messages

---
+++ ChatMessage.java
@@ -0,0 +1,39 @@
+import java.io.*;
+/*
+ * This class defines the different type of messages that will be exchanged between the
+ * Clients and the Server. 
+ *  Java Client to a Java Server is lot easier to pass Java objects, no 
+ * need to count bytes or to wait for a line feed at the end of the frame
+ */
+public class ChatMessage implements Serializable {
+
+	protected static final long serialVersionUID = 1112122200L;
+
+	// The different types of message sent by the Client
+	// WHOISIN to receive the list of the users connected
+	// MESSAGE an ordinary message
+	// LOGOUT to disconnect from the Server
+	static final int WHOISIN = 0, MESSAGE = 1, LOGOUT = 2;
+	private int type;
+	private String message;
+
+    //default constructor
+    ChatMessage(){
+        this.type = null;
+        this.message = null;
+    }
+	
+	// constructor
+	ChatMessage(int type, String message) {
+		this.type = type;
+		this.message = message;
+	}
+	
+	// getters
+	int getType() {
+		return type;
+	}
+	String getMessage() {
+		return message;
+	}
+}
